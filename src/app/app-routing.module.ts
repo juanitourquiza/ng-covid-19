9c439d13241f975b8bfc2572839a4103f9142016
JuanUrquiza <macbookpro@iMacJuanUrquiza.local>
2020-03-30 17:36:11 -0500

    se agrega rutas, se agrega informacion de google, se cambia datos de links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,26 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
 export const routes: Routes = [
   {
-    loadChildren: () => import('./coronavirus/coronavirus.module')
-      .then(m => m.CoronavirusModule),
-    path: ''
+    loadChildren: () =>
+      import("./coronavirus/coronavirus.module").then(m => m.CoronavirusModule),
+    path: ""
   },
   {
-    loadChildren: () => import('./info/info.module')
-      .then(m => m.InfoModule),
-    path: 'infos'
+    loadChildren: () => import("./info/info.module").then(m => m.InfoModule),
+    path: "infos"
   }
 ];
 
 @NgModule({
   exports: [RouterModule],
-  imports: [RouterModule.forRoot(routes, { onSameUrlNavigation: 'reload', initialNavigation: 'enabled' })]
+  imports: [
+    RouterModule.forRoot(routes, {
+      onSameUrlNavigation: "reload",
+      initialNavigation: "enabled",
+      useHash: true
+    })
+  ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
